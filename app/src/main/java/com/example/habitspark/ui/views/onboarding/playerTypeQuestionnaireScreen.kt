6ad4b494dab3986dea43f1e9fe48219455ac9533
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-07-20 02:52:20 +0200

    Extract Input fields to dedicated helper function and refactor usages

--- app/src/main/java/com/example/habitspark/ui/views/onboarding/playerTypeQuestionnaireScreen.kt
+++ app/src/main/java/com/example/habitspark/ui/views/onboarding/playerTypeQuestionnaireScreen.kt
@@ -1,5 +1,7 @@
 package com.example.habitspark.ui.views.onboarding
 
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -27,13 +30,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.ui.platform.LocalContext
 
 
 data class PlayerTypeResult(
